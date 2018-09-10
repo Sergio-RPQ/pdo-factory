ff46d12d5050af2e330087ace6ae61ce7f369e57
Sergio Ricardo <desenvolvimento@altaredesistemas.com.br>
2018-09-10 17:49:00 -0300

    Atualização 10/09/2018 17:48

--- ConnectionFactory.php
+++ ConnectionFactory.php
@@ -1,5 +1,4 @@
 <?php
-
 	require_once 'config.php';
 	
 	class ConnectionFactory{
@@ -22,34 +21,20 @@
 							self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 					        break;
 					    case 'pgsql':
-					    	self::$connection = null;
+					    	self::$connection = new PDO(DB_DRIVER.':host='.DB_HOSTNAME.';port='.DB_PORT.';dbname='.DB_DATABASE, DB_USER, DB_PASSWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_EMULATE_PREPARES => false));
+					    	self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            				self::$connection->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+            				self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 					    	break;
 					    case 'mssql':
-					    	self::$connection = null;
+					    	self::$connection = new PDO(DB_DRIVER.':host='.DB_HOSTNAME.';port='.DB_PORT.';dbname='.DB_DATABASE, DB_USER, DB_PASSWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_EMULATE_PREPARES => false));
+					    	self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            				self::$connection->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+            				self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 					    	break;	
 					    default:
 					       self::$connection = null;
 					}
-					/*
-					if(DB_DRIVER == 'mysql'){
-						self::$connection = new PDO(DB_DRIVER.':host='.DB_HOSTNAME.';dbname='.DB_DATABASE, DB_USER, DB_PASSWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", 
-											 PDO::ATTR_EMULATE_PREPARES => false));
-
-						self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            			self::$connection->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
-            			self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-					}
-					else if(DB_DRIVER == 'sqlite'){
-						self::$connection = new PDO(DB_DRIVER.":".DB_HOSTNAME.DB_DATABASE) 
-											or die("Erro ao abrir a base");
-						self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-						self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);					
-					}
-					else if(DB_DRIVER == 'pgsql'){
-						//self::$connection = new PDO(DB_DRIVER.":");
-						//$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
-					}
-					*/	
 				}
 				catch(PDOException $e){
 					echo $e->getMessage();
@@ -61,5 +46,4 @@
 		public static function getDisconnect(){
 			self::$connection = null;
 		}
-
 	}
